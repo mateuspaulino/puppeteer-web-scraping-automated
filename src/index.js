f22ea4eaf3a93f1e7605fb5bb20fc8a57717b0e3
Mateus Paulino <mateus.paulino@grey.com>
2019-05-22 15:14:10 -0300

    executing webscraping alone

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-const puppeteer = require('puppeteer');
-let bookingUrl = 'http://www.profnit.org.br/pt/sample-page/';
+const puppeteer = require('puppeteer')
+let bookingUrl = 'http://www.profnit.org.br/pt/sample-page/'
 
-const webscraping = (async () => {
+const webscraping = async () => {
 	const browser = await puppeteer.launch({
 		headless: true
-	});
-	const page = await browser.newPage();
-	let dataObj = {};
+	})
+	const page = await browser.newPage()
+	let dataObj = {}
 
 	try {
-		await page.goto(bookingUrl);
+		await page.goto(bookingUrl)
 
 		let publishedNews = await page.evaluate(() => {
-			let newsDOM = document.querySelectorAll('li.cat-post-item');
-			let newsList = [];
+			let newsDOM = document.querySelectorAll('li.cat-post-item')
+			let newsList = []
 			newsDOM.forEach((linkElement) => {
-				const currentNews = linkElement.querySelector('a.cat-post-title').innerText;
-				newsList.push(currentNews);
-			});
-			return newsList;
-		});
+				const currentNews = linkElement.querySelector('a.cat-post-title').innerText
+				newsList.push(currentNews)
+			})
+			return newsList
+		})
 
 		dataObj = {
 			amount: publishedNews.length,
 			publishedNews
-		};
-		console.log(dataObj);
+		}
+		console.log(dataObj)
 	} catch (e) {
 		console.log(e)
 	}
 
 	browser.close()
-})()
+	return dataObj
+}
+
+webscraping()
+
