e5d1eb08277ce98a290a1d832fff216476c9d003
Mateus Paulino <mateus.paulino@grey.com>
2019-05-22 15:09:27 -0300

    auto-executing webscraping

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 const puppeteer = require('puppeteer');
-
 let bookingUrl = 'http://www.profnit.org.br/pt/sample-page/';
-(async () => {
+
+const webscraping = (async () => {
 	const browser = await puppeteer.launch({
 		headless: true
 	});
 	const page = await browser.newPage();
-	await page.goto(bookingUrl);
+	let dataObj = {};
 
-	let publishedNews = await page.evaluate(() => {
-		let newsDOM = document.querySelectorAll('li.cat-post-item');
-		let newsList = [];
-		newsDOM.forEach((linkElement) => {
-			const currentNews = linkElement.querySelector('a.cat-post-title').innerText;
-			newsList.push(currentNews);
+	try {
+		await page.goto(bookingUrl);
+
+		let publishedNews = await page.evaluate(() => {
+			let newsDOM = document.querySelectorAll('li.cat-post-item');
+			let newsList = [];
+			newsDOM.forEach((linkElement) => {
+				const currentNews = linkElement.querySelector('a.cat-post-title').innerText;
+				newsList.push(currentNews);
+			});
+			return newsList;
 		});
-		return newsList;
-	});
 
-	let dataObj = {
-		amount: publishedNews.length,
-		publishedNews
-	};
+		dataObj = {
+			amount: publishedNews.length,
+			publishedNews
+		};
+		console.log(dataObj);
+	} catch (e) {
+		console.log(e)
+	}
 
-	console.log(dataObj);
 	browser.close()
-})();
+})()
